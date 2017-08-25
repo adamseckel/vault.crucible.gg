2de7e50f2fc6bb1f82cbe4a6a224e65700be63e7
hemlok <adam.seckel@gmail.com>
2017-08-24 21:44:17 -0700

    test sw cache on prod

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,25 @@
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open('crucible.gg-icons').then(function(cache) {
       return cache.addAll(
         ['https://fonts.googleapis.com/icon?family=Material+Icons']
       );
     })
   );
+});
+
+self.addEventListener('fetch', function fetcher (event) {
+  var request = event.request;
+  // check if request 
+  if (request.url.indexOf('destiny_content/icons') > -1) {
+    // contentful asset detected
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        // return from cache, otherwise fetch from network
+        return response || fetch(request);
+      })
+    );
+  }
+  // otherwise: ignore event
 });
