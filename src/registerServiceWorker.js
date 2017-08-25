eb8f81f4c87faf929fb93f5dce56119af50fd835
hemlok <adam.seckel@gmail.com>
2017-08-24 21:39:28 -0700

    remove unnecessary css bundle, start service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -30,7 +30,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
