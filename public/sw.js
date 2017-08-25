42f9ec73613c68eebdc3c15d8cb304fbde532432
hemlok <adam.seckel@gmail.com>
2017-08-24 22:13:44 -0700

    test sw cache on prod

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('crucible.gg-icons').then(function(cache) {
+    caches.open('fonts').then(function(cache) {
       return cache.addAll(
         ['https://fonts.googleapis.com/icon?family=Material+Icons']
       );
@@ -13,15 +13,15 @@ self.addEventListener('fetch', function fetcher (event) {
   var request = event.request;
   // check if request 
   if (request.url.indexOf('destiny_content/icons') > -1) {
-    console.log('bungie request', request.url)
     // contentful asset detected
     event.respondWith(
       caches.match(event.request).then(function(response) {
         // return from cache, otherwise fetch from network
-        return response || fetch(request).then((image) => {
+        return response || fetch(request).then(response => {
           return caches.open('icons')
             .then(cache => {
-              cache.add(request);
+              cache.put(event.request, response.clone());
+              return response;
             });
         });
       })
